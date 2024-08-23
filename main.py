4044c8eb516ee717c252d051d9c1552ba64649bc
Marcin Bogusz <bogusz.marcin@icloud.com>
2024-08-23 08:45:02 +0200

    Turtle sketch project is ready.

--- main.py
+++ main.py
@@ -7,31 +7,36 @@ screen = Screen()
 
 def forward():
     """Function will move forward the object by 10"""
-    return tim.forward(10)
+    tim.forward(10)
 
 
 def right():
-    pass
+    """Function will turn right the object by 15 degree"""
+    tim.right(15)
 
 
 def left():
-    pass
+    """Function will turn left the object by 15 degree"""
+    tim.left(15)
 
 
 def backward():
-    pass
+    """Function will move backward the object by 10"""
+    tim.backward(10)
 
 
 def clear():
-    """Function will clear the screen"""
-    return t.clear()
-
+    """Function will clear the screen and set the object position to 0, 0"""
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
 
 
 screen.listen()
-screen.onkey(key="c", fun=clear)
-screen.onkey(key="w", fun=forward)
-screen.onkey(key="s", fun=backward)
-screen.onkey(key="d", fun=right)
-screen.onkey(key="a", fun=left)
+screen.onkeypress(key="w", fun=forward)
+screen.onkeypress(key="s", fun=backward)
+screen.onkeypress(key="d", fun=right)
+screen.onkeypress(key="a", fun=left)
+screen.onkeypress(key="c", fun=clear)
 screen.exitonclick()
